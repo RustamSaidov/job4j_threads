747e5fc30b0f4eccb7e850617e581ce422ed2a02
Qafas <rustam_ussr@mail.ru>
2022-10-23 12:17:58 +0400

    1.1. Состояние нити. [#229175]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,5 +1,7 @@
 package ru.job4j.concurrent;
 
+import java.util.concurrent.TimeUnit;
+
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
@@ -11,6 +13,11 @@ public class ThreadState {
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            try {
+                TimeUnit.SECONDS.sleep(0);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         System.out.println("Work of additional threads has ends.");
     }
