ca319c86b851411d521835287dd7caf8fc9e2a3e
Qafas <rustam_ussr@mail.ru>
2022-10-23 20:37:26 +0400

    3. Прерывание нити [#1019]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -1,32 +1,35 @@
-/*package ru.job4j.concurrent;
+package ru.job4j.concurrent;
 
 public class ConsoleProgress implements Runnable {
-
     public static void main(String[] args) {
         Thread progress = new Thread(new ConsoleProgress());
-        progress.start();
         try {
-            Thread.sleep(1000);
+            progress.start();
+            Thread.sleep(10000);
+            progress.interrupt();
+
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        progress.interrupt();
     }
 
     @Override
     public void run() {
         int i = 0;
-        char[] arrayChar = {'|', '/', '\"'};
-        while (!Thread.currentThread().isInterrupted()) {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        char[] process = {'|', '/', '\\'};
+        try {
+            Thread.sleep(500);
+            while (!Thread.currentThread().isInterrupted()) {
+                if (i == process.length) {
+                    i = 0;
+                }
+                System.out.println("Loading ... " + process[i]);
+                i++;
             }
-
-            System.out.print("\r Loading ... " + arrayChar[i++]);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
-*/
+
 
