7dbfb0feab523012947d3fdfd24ddbcbc4241bde
rusta <rustam_ussr@mail.ru>
2022-10-24 08:21:12 +0300

    3. Прерывание нити [#1019 #354230]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -1,16 +1,11 @@
 package ru.job4j.concurrent;
 
 public class ConsoleProgress implements Runnable {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
-        try {
             progress.start();
             Thread.sleep(10000);
             progress.interrupt();
-
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @Override
