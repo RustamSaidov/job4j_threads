cbc9c5db02a60e8a8674cadc7fdc811bb8f2dd8f
rusta <rustam_ussr@mail.ru>
2022-11-01 13:48:49 +0300

    1. Реализовать шаблон Producer Consumer. [#1098]

--- src/main/java/ru/job4j/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -8,67 +8,51 @@ import java.util.Queue;
 
 @ThreadSafe
 public class SimpleBlockingQueue<T> {
-    final int steps = 14;
-    final int queueMaxLength = 5;
-    int totalCount = 0;
-    int count = 0;
-
+    int limit = 10;
 
     @GuardedBy("this")
     private final Queue<T> queue = new LinkedList<>();
 
-    public synchronized void offer(T value) {
-        while (queue.size() >= queueMaxLength || totalCount == steps) {
-            try {
-                this.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    public synchronized void offer(T value) throws InterruptedException {
+        while (queue.size() == limit) {
+            wait();
         }
         queue.add(value);
-        System.out.println("value " + value + " is added");
-        count++;
-        totalCount++;
         notifyAll();
     }
 
-    public synchronized T poll() {
+    public synchronized T poll() throws InterruptedException {
         while (queue.size() == 0) {
-            try {
-                count = 0;
-                notifyAll();
-                this.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            wait();
         }
-        T r = queue.poll();
-        System.out.println("value " + r + " is got");
-        return r;
+        T result = queue.poll();
+        notifyAll();
+        return result;
 
     }
 
     public static void main(String[] args) throws InterruptedException {
         SimpleBlockingQueue<Integer> sbq = new SimpleBlockingQueue<>();
-        Thread first = new Thread(
-                () -> {
-                    while (sbq.totalCount < sbq.steps) {
-                        sbq.offer(sbq.totalCount);
-                    }
-                    System.out.println("first ended");
-                },
-                "first"
+        Thread first = new Thread(() -> {
+            while (sbq.queue.size() < sbq.limit) {
+                try {
+                    sbq.offer(sbq.queue.size());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "first"
 
         );
-        Thread second = new Thread(
-                () -> {
-                    while (sbq.totalCount < sbq.steps || !sbq.queue.isEmpty()) {
-                        sbq.poll();
-                    }
-                    System.out.println("second ended");
-                },
-                "second"
-        );
+        Thread second = new Thread(() -> {
+            while (sbq.queue.size() < sbq.limit) {
+                try {
+                    sbq.poll();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "second");
         first.start();
         second.start();
         first.join();
