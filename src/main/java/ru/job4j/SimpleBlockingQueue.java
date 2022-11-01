d31774d70f6a13220f757cfcdb2ed40a3722290b
rusta <rustam_ussr@mail.ru>
2022-11-01 09:48:50 +0300

    1. Реализовать шаблон Producer Consumer. [#1098]

--- src/main/java/ru/job4j/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -8,16 +8,16 @@ import java.util.Queue;
 
 @ThreadSafe
 public class SimpleBlockingQueue<T> {
-    final int steps = 10;
+    final int steps = 14;
     final int queueMaxLength = 5;
-    int totalcount=0;
+    int totalCount = 0;
     int count = 0;
 
     @GuardedBy("this")
     private final Queue<T> queue = new LinkedList<>();
 
     public synchronized void offer(T value) {
-        while (queue.size()>=1) {
+        while (queue.size() >= queueMaxLength || totalCount == steps) {
             try {
                 this.wait();
             } catch (InterruptedException e) {
@@ -25,38 +25,46 @@ public class SimpleBlockingQueue<T> {
             }
         }
         queue.add(value);
+        System.out.println("value " + value + " is added");
+        count++;
+        totalCount++;
+        notifyAll();
     }
 
     public synchronized T poll() {
         while (queue.size() == 0) {
             try {
-                count=0;
+                count = 0;
+                notifyAll();
                 this.wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        return queue.poll();
+        T r = queue.poll();
+        System.out.println("value " + r + " is got");
+        return r;
+
     }
 
     public static void main(String[] args) throws InterruptedException {
-        SimpleBlockingQueue sbq = new SimpleBlockingQueue<Integer>();
+        SimpleBlockingQueue<Integer> sbq = new SimpleBlockingQueue<>();
         Thread first = new Thread(
                 () -> {
-                    while (sbq.totalcount < sbq.steps) {
-                        sbq.offer(sbq.totalcount);
-                        System.out.println(Thread.currentThread().getName() + " added " + sbq.totalcount + " value");
-                        sbq.count++;
-                        sbq.totalcount++;
+                    while (sbq.totalCount < sbq.steps) {
+                        sbq.offer(sbq.totalCount);
                     }
+                    System.out.println("first ended");
                 },
                 "first"
+
         );
         Thread second = new Thread(
                 () -> {
-                    while (sbq.totalcount < sbq.steps) {
-                        System.out.println(Thread.currentThread().getName() + " get " + sbq.poll() + " value");
+                    while (sbq.totalCount < sbq.steps || !sbq.queue.isEmpty()) {
+                        sbq.poll();
                     }
+                    System.out.println("second ended");
                 },
                 "second"
         );
@@ -64,6 +72,5 @@ public class SimpleBlockingQueue<T> {
         second.start();
         first.join();
         second.join();
-
     }
 }
--- src/test/java/ru/job4j/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class SimpleBlockingQueueTest {
 
     @Test
-    public void add(){
-        
+    public void add() {
+
     }
 }
