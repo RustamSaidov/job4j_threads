fbd69fc0720eb6f7f98e1d0584b5c20c282e1aa8
rusta <rustam_ussr@mail.ru>
2022-11-03 08:24:53 +0300

    2. Обеспечить остановку потребителя. [#66825]

--- src/main/java/ru/job4j/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -1,24 +1,24 @@
 package ru.job4j.buffer;
 
 import ru.job4j.SimpleBlockingQueue;
+
 public class ParallelSearch {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
         final int maxIndex = 3;
-        final int[] count = {0};
         final Thread consumer = new Thread(
                 () -> {
-                    while (count[0] < maxIndex && queue.getQueue().isEmpty()) {
+                    while (!queue.getQueue().isEmpty() ||
+                            !Thread.currentThread().isInterrupted()
+                    ) {
                         try {
                             System.out.println(queue.poll());
-                            count[0]++;
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                             Thread.currentThread().interrupt();
                         }
                     }
-                    Thread.currentThread().interrupt();
                 }
         );
         consumer.start();
@@ -36,8 +36,11 @@ public class ParallelSearch {
                             e.printStackTrace();
                         }
                     }
+                    consumer.interrupt();
                 }
         );
         producer.start();
+        producer.join();
+        consumer.join();
     }
 }
