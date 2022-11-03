db793f1709d722b8ca533d6f872562262aaff8ad
rusta <rustam_ussr@mail.ru>
2022-11-03 14:46:47 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/main/java/ru/job4j/threads/threadlocal/ThreadLocalDemo.java
+++ src/main/java/ru/job4j/threads/threadlocal/ThreadLocalDemo.java
@@ -1,13 +1,13 @@
 package ru.job4j.threads.threadlocal;
 
 public class ThreadLocalDemo {
-    static ThreadLocal<String> tl = new ThreadLocal<>(); // объявляем статическую переменную ThreadLocal
+    static ThreadLocal<String> tl = new ThreadLocal<>(); /* объявляем статическую переменную ThreadLocal */
 
     public static void main(String[] args) throws InterruptedException {
         Thread first = new FirstThread();
         Thread second = new SecondThread();
-        tl.set("Это поток main."); // записываем данные в переменную ThreadLocal
-        System.out.println(tl.get()); // получаем данные из этой переменной и выводим на печать
+        tl.set("Это поток main."); /* записываем данные в переменную ThreadLocal */
+        System.out.println(tl.get()); /* получаем данные из этой переменной и выводим на печать */
         first.start();
         second.start();
         first.join();
