82809b2bedbf053a29b2d9e344b0ad2f42ff1eec
rusta <rustam_ussr@mail.ru>
2022-11-14 08:30:38 +0300

    1. Реализовать ThreadPool [#1099]

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -15,32 +15,15 @@ public class ThreadPool {
             Thread thread = new Thread();
             thread.start();
             threads.add(thread);
-            System.out.println(12345);
         }
-        System.out.println(111);
         threads.forEach(t -> {
             while (!Thread.currentThread().isInterrupted()) {
-                System.out.println(999);
-                while(!tasks.getQueue().isEmpty()){
+                while (!tasks.getQueue().isEmpty()) {
                     tasks.getQueue().poll().run();
                 }
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
             }
-            System.out.println(222);
-//            while (!tasks.getQueue().isEmpty() || !Thread.currentThread().isInterrupted()) {
-//                if (t.getState() == Thread.State.WAITING) {
-//                    t.notify();
-//                }
-//                Runnable task = tasks.getQueue().poll();
-//                t = new Thread(task);
-//                t.start();
-//            }
         });
-}
+    }
 
     public void work(Runnable job) throws InterruptedException {
         tasks.offer(job);
@@ -53,7 +36,6 @@ public class ThreadPool {
     public static void main(String[] args) throws InterruptedException {
         final int[] i = {0};
         ThreadPool threadPool = new ThreadPool();
-        System.out.println("!");
         Runnable job1 = () -> {
             System.out.println(i[0]);
             i[0] = i[0] + 1;
@@ -85,15 +67,12 @@ public class ThreadPool {
             Thread.currentThread().interrupt();
         };
         threadPool.work(job1);
-        System.out.println("!");
         threadPool.work(job2);
         threadPool.work(job3);
         threadPool.work(job4);
         threadPool.work(job5);
         threadPool.work(job6);
-        System.out.println("!!@#$$$$$$%");
         threadPool.shutdown();
-        System.out.println("!!@#$$$$$$%");
         threadPool.threads.forEach(t -> {
             try {
                 t.join();
