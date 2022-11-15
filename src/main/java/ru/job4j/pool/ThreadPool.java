fe6d95214d23c8addd8639beb7351922c375308e
rusta <rustam_ussr@mail.ru>
2022-11-15 08:51:24 +0300

    3.1. Добавить изменения в последний коммит. [#504829]

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -1,16 +1,17 @@
-/*package ru.job4j.pool;
+package ru.job4j.pool;
 
 import ru.job4j.SimpleBlockingQueue;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadPool {
     private final int size = Runtime.getRuntime().availableProcessors();
     private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size * 10);
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size);
 
-    public ThreadPool() throws InterruptedException {
+    public ThreadPool() {
         for (int i = 0; i < size; i++) {
             Thread thread = new Thread(
                     () -> {
@@ -18,11 +19,10 @@ public class ThreadPool {
                             try {
                                 tasks.poll().run();
                             } catch (InterruptedException e) {
-                                e.printStackTrace();
+                                Thread.currentThread().interrupt();
                             }
                         }
                     }
-
             );
             thread.start();
             threads.add(thread);
@@ -38,31 +38,25 @@ public class ThreadPool {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        final int[] i = {0};
+        AtomicInteger j = new AtomicInteger();
         ThreadPool threadPool = new ThreadPool();
         Runnable job1 = () -> {
-            System.out.println(i[0]);
-            i[0] = i[0] + 1;
+            System.out.println("This is thread with the name " + Thread.currentThread().getName() + " and it outputs value " + j.getAndIncrement());
         };
         Runnable job2 = () -> {
-            System.out.println(i[0]);
-            i[0] = i[0] + 1;
+            System.out.println("This is thread with the name " + Thread.currentThread().getName() + " and it outputs value " + j.getAndIncrement());
         };
         Runnable job3 = () -> {
-            System.out.println(i[0]);
-            i[0] = i[0] + 1;
+            System.out.println("This is thread with the name " + Thread.currentThread().getName() + " and it outputs value " + j.getAndIncrement());
         };
         Runnable job4 = () -> {
-            System.out.println(i[0]);
-            i[0] = i[0] + 1;
+            System.out.println("This is thread with the name " + Thread.currentThread().getName() + " and it outputs value " + j.getAndIncrement());
         };
         Runnable job5 = () -> {
-            System.out.println(i[0]);
-            i[0] = i[0] + 1;
+            System.out.println("This is thread with the name " + Thread.currentThread().getName() + " and it outputs value " + j.getAndIncrement());
         };
         Runnable job6 = () -> {
-            System.out.println(i[0]);
-            i[0] = i[0] + 1;
+            System.out.println("This is thread with the name " + Thread.currentThread().getName() + " and it outputs value " + j.getAndIncrement());
         };
         threadPool.work(job1);
         threadPool.work(job2);
@@ -71,13 +65,5 @@ public class ThreadPool {
         threadPool.work(job5);
         threadPool.work(job6);
         threadPool.shutdown();
-        threadPool.threads.forEach(t -> {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
     }
 }
-*/
