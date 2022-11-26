2de348d470a06682faf6305e9ba6e92e56249fc6
rusta <rustam_ussr@mail.ru>
2022-11-26 14:18:53 +0300

    4. CompletableFuture [#361626]

--- src/main/java/ru/job4j/pools/RolColSum.java
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -1,10 +1,18 @@
-/*
 package ru.job4j.pools;
+
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class RolColSum {
     public static class Sums {
         private int rowSum;
         private int colSum;
+
+        public Sums(int rowSum, int colSum) {
+            this.rowSum = rowSum;
+            this.colSum = colSum;
+        }
 
         public int getRowSum() {
             return rowSum;
@@ -21,30 +29,55 @@ public class RolColSum {
         public void setColSum(int colSum) {
             this.colSum = colSum;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof Sums sums)) {
+                return false;
+            }
+            return getRowSum() == sums.getRowSum() && getColSum() == sums.getColSum();
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getRowSum(), getColSum());
+        }
     }
 
     public static Sums[] sum(int[][] matrix) {
         Sums[] resultArray = new Sums[matrix.length];
+
         for (int i = 0; i < matrix.length; i++) {
-            Sums sums = resultArray[i];
+            resultArray[i] = new Sums(0, 0);
             for (int j = 0; j < matrix[0].length; j++) {
-                sums.setRowSum(sums.getRowSum()+matrix[i][j]);
-            }
-        }
-        for (int j = 0; j < matrix.length; j++) {
-            Sums sums = resultArray[j];
-            for (int i = 0; i < matrix[0].length; i++) {
-                sums.setRowSum(sums.getRowSum()+matrix[j][i]);
+                resultArray[i].setRowSum(resultArray[i].getRowSum() + matrix[i][j]);
+                resultArray[i].setColSum(resultArray[i].getColSum() + matrix[j][i]);
             }
         }
         return resultArray;
     }
 
-    public static void main(String[] args) {
-        int[][] array = new int[][]
+    public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
+        Sums[] resultArray = new Sums[matrix.length];
 
+        for (int i = 0; i < matrix.length; i++) {
+            resultArray[i] = calcRowSumAndColSum(new Sums(0, 0), matrix, i).get();
+        }
+        return resultArray;
     }
 
+    public static CompletableFuture<Sums> calcRowSumAndColSum(Sums sum, int[][] matrix, int i) {
+        return CompletableFuture.supplyAsync(
+                () -> {
+                    for (int j = 0; j < matrix[0].length; j++) {
+                        sum.setRowSum(sum.getRowSum() + matrix[i][j]);
+                        sum.setColSum(sum.getColSum() + matrix[j][i]);
+                    }
+                    return sum;
+                }
+        );
+    }
 }
-
- */
---
+++ src/test/java/ru/job4j/pools/RolColSumTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.pools;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RolColSumTest {
+
+    @Test
+    void whenUseSyncSumming() {
+        int[][] array = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+        };
+        RolColSum.Sums[] testArray = {
+                new RolColSum.Sums(6, 12),
+                new RolColSum.Sums(15, 15),
+                new RolColSum.Sums(24, 18),
+        };
+        RolColSum.Sums[] resultArray = RolColSum.sum(array);
+        assertArrayEquals(testArray, resultArray);
+    }
+
+    @Test
+    void whenUseAsyncSumming() throws ExecutionException, InterruptedException {
+        int[][] array = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+        };
+        RolColSum.Sums[] testArray = {
+                new RolColSum.Sums(6, 12),
+                new RolColSum.Sums(15, 15),
+                new RolColSum.Sums(24, 18),
+        };
+        RolColSum.Sums[] resultArray = RolColSum.asyncSum(array);
+        assertArrayEquals(testArray, resultArray);
+    }
+}
